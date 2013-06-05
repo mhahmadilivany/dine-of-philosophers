19d5f03eb5eba784977bdc1353af42e22b7b8662
mhahmadilivany <mojtabaahmadi.redsmart@yahoo.com>
2013-06-05 15:36:47 +0430

    Update main.cpp

--- dine-of-philosephers/main.cpp
+++ dine-of-philosephers/main.cpp
@@ -7,11 +7,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Widget w;
     w.show();
-    QVector<int>d;
-    d[0]=1;
-    d[1]=2;
-    d[2]=3;
-
-    
     return a.exec();
 }
